408425cf94654bbdb4c1bdb124bbd836f990500a
georgiaboy02 <tbrewer1@mail.mvnu.edu>
2015-05-09 14:22:14 -0400

    added for loop for fizzbuzz

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 $(document).ready(function() {
 	var submit = ".submit";
 	$(submit).mousedown(function() {
-		console.log("click");
 		var value = $(".input-text").val();
 		var valNumber = +value;
 		if (isNaN(valNumber)) {
-			console.log("Please log a number");
+			alert("Please enter a real number!");
 			console.log(typeof value);
+		}
+		else {
+			for (var i = 1; i <= valNumber; i++) {
+				if ((i % 3 == 0) && (i % 5 == 0)) {
+					console.log("Fizzbuzz");
+				}
+				else if (i % 3 == 0) {
+					console.log("Fizz");
+				}
+				else if (i % 5 == 0) {
+					console.log("Buzz");
+				}
+				else {
+					console.log(i);
+				}
+			}
 		}
 	});
 });
